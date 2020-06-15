76555af934f6b9cc90ef1a97531c130b2c073043
Romanov5000 <romanovvladislav97@gmail.com>
2020-06-15 16:56:12 +0300

    new style and photo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let mainSlider = document.querySelector('.main_slider');
 mainSlider.lightSlider({
     slides: 2,
     slidesWidth: 500,
-    slidesHeight: 300,
+    slidesHeight: 280,
     nav: true,
     autoplay: false,
     autoplaySpeed: 2000,
